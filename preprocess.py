7eda326e73c115696caee8ace9b9ce45b78f19aa
Fabian Lindner <lindner@vaw.baug.ethz.ch>
2015-11-27 10:42:30 +0100

    added option1: bandpass filter

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,14 @@ from scipy import signal
 import numpy as np
 
 
-# nearest powof2 which is smaller than number
 def nearest_powof2(number):
+    """
+    This function determines the next smaller value to "number" which is of power 2
+    :type number: interger
+    :param number: number of samples
+
+    :return: next smaller value of power 2 which is smaller than number
+    """
     x = np.arange(0, 27)
     pow2 = 2**x
     ind = np.argmin(abs(number - pow2))
@@ -14,10 +20,32 @@ def nearest_powof2(number):
     return res
 
 
-def preprocess(s, prepr, Fs, fc_min, fc_max, taper_fract):
-    matr = s[0]
+def preprocess(matr, prepr, Fs, fc_min, fc_max, taper_fract):
+    """
+    :type matr: numpy.ndarray
+    :param matr: time series of used stations (dim: [number of samples, number of stations])
+    :type prepr: integer
+    :param prepr: type of preprocessing. 0=None, 1=bandpass filter, 2=spectral whitening
+    :type Fs: float
+    :param Fs: sampling rate of data streams
+    :type fc_min, fc_max: float
+    :param fc_min, fc_max: corner frequencies used for preprocessing
+    :type taper_fract: float
+    :param taper_fract: percentage of frequency band which is tapered after spectral whitening
+
+    :return: preprocessed data (dim: [number of samples, number of stations])
+    """
     if prepr == 0:
         data = signal.detrend(matr, axis=0)
+
+    elif prepr == 1:
+        # generate frequency vector and butterworth filter
+        b, a = signal.butter(4, np.array([fc_min, fc_max]) / Fs * 2, btype="bandpass")
+        # filter data and normalize it by maximum energy
+        data = signal.filtfilt(b, a, signal.detrend(matr, axis=0), axis=0)
+        fact = np.sqrt(np.dot(np.ones((data.shape[0], 1)), np.sum(data**2, axis=0).reshape((1, data.shape[1]))))
+        data = np.divide(data, fact)
+
     elif prepr == 2:
         nfft = nearest_powof2(matr.shape[0])
         Y = np.fft.fft(matr, n=nfft, axis=0)
@@ -35,16 +63,16 @@ def preprocess(s, prepr, Fs, fc_min, fc_max, taper_fract):
         for ii in range(Y.shape[1]):
             if len(Jdebut[0]) > 1:
                 Y_white[Jdebut, ii] = np.multiply(Y_white[Jdebut, ii],
-                                        np.sin(np.pi / 2 * np.arange(0, len(Jdebut[0])) / len(Jdebut[0]))**2)
+                            np.sin(np.pi / 2 * np.arange(0, len(Jdebut[0])) / len(Jdebut[0]))**2)
             if len(Jfin[0]) > 1:
                 Y_white[Jfin, ii] = np.multiply(Y_white[Jfin, ii],
-                                        np.cos(np.pi / 2 * np.arange(0, len(Jfin[0])) / len(Jfin[0]))**2)
+                            np.cos(np.pi / 2 * np.arange(0, len(Jfin[0])) / len(Jfin[0]))**2)
 
         # perform inverse fft to obtain time signal
-        #data = 2*np.real(np.fft.ifft(Y_white, n=nfft, axis=0))
+        # data = 2*np.real(np.fft.ifft(Y_white, n=nfft, axis=0))
         data = np.fft.ifft(Y_white, n=nfft, axis=0)
         # normalize it by maximum energy
-        fact = np.sqrt(np.dot(np.ones((data.shape[0],1)), np.sum(data**2, axis=0).reshape((1, data.shape[1]))))
+        fact = np.sqrt(np.dot(np.ones((data.shape[0], 1)), np.sum(data**2, axis=0).reshape((1, data.shape[1]))))
         data = np.divide(data, fact)
 
     return data
